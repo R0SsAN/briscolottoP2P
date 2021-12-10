d2a68b33c229098d1a3c514d06177f03ce87e322
Luca Rossini <66002235+R0SsAN@users.noreply.github.com>
2021-12-10 09:49:24 +0100

    Inserimento iniziale classi
    Inserimento iniziale delle classi:
    - Carta
    - Gestione_Briscola

---
+++ briscolottoP2P/briscolottoP2P/Carta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace briscolottoP2P
+{
+    class Carta
+    {
+        public string seme { get; set; }
+        public int valore { get; set; }
+        public int punteggio { get; set; }
+        public string img { get; set; }
+
+        public Carta(string seme, int valore, int punteggio, string img)
+        {
+            this.seme = seme;
+            this.valore = valore;
+            this.punteggio = punteggio;
+            this.img = img;
+        }
+
+    }
+}
---
+++ briscolottoP2P/briscolottoP2P/GestioneBriscola.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace briscolottoP2P
+{
+    class GestioneBriscola
+    {
+        //singleton
+        static GestioneBriscola _instance = null;
+        static public GestioneBriscola getInstance(MainWindow w)
+        {
+            if (_instance == null)
+                _instance = new GestioneBriscola(w);
+            return _instance;
+        }
+        //VARIABILI
+
+
+
+        public GestioneBriscola(MainWindow w)
+        {
+
+        }
+
+
+    }
+}
